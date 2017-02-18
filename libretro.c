6417f689dc847d7bf6f99b704fa664564dc040e1
twinaphex <libretro@gmail.com>
2017-02-18 17:14:03 +0100

    Bump up version

--- libretro.c
+++ libretro.c
@@ -384,7 +384,7 @@ void retro_get_system_info(struct retro_system_info *info)
 #ifndef GIT_VERSION
 #define GIT_VERSION ""
 #endif
-   info->library_version = "1.3.2.3" GIT_VERSION;
+   info->library_version = "1.3.2.4" GIT_VERSION;
    info->need_fullpath = true;
    info->valid_extensions = "iso";
 }
