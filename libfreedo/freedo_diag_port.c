444112529252ffd109570f95d306f6d36bb5f800
Antonio SJ Musumeci <trapexit@spawn.link>
2018-05-06 17:23:36 -0400

    reindent diag_port.{c,h}

--- libfreedo/freedo_diag_port.c
+++ libfreedo/freedo_diag_port.c
@@ -1,25 +1,25 @@
 /*
-   www.freedo.org
-   The first and only working 3DO multiplayer emulator.
+  www.freedo.org
+  The first and only working 3DO multiplayer emulator.
 
-   The FreeDO licensed under modified GNU LGPL, with following notes:
+  The FreeDO licensed under modified GNU LGPL, with following notes:
 
- *   The owners and original authors of the FreeDO have full right to develop closed source derivative work.
- *   Any non-commercial uses of the FreeDO sources or any knowledge obtained by studying or reverse engineering
- of the sources, or any other material published by FreeDO have to be accompanied with full credits.
- *   Any commercial uses of FreeDO sources or any knowledge obtained by studying or reverse engineering of the sources,
- or any other material published by FreeDO is strictly forbidden without owners approval.
+  *   The owners and original authors of the FreeDO have full right to develop closed source derivative work.
+  *   Any non-commercial uses of the FreeDO sources or any knowledge obtained by studying or reverse engineering
+  of the sources, or any other material published by FreeDO have to be accompanied with full credits.
+  *   Any commercial uses of FreeDO sources or any knowledge obtained by studying or reverse engineering of the sources,
+  or any other material published by FreeDO is strictly forbidden without owners approval.
 
- The above notes are taking precedence over GNU LGPL in conflicting situations.
+  The above notes are taking precedence over GNU LGPL in conflicting situations.
 
- Project authors:
+  Project authors:
 
- Alexander Troosh
- Maxim Grishin
- Allen Wright
- John Sammons
- Felix Lazarev
- */
+  Alexander Troosh
+  Maxim Grishin
+  Allen Wright
+  John Sammons
+  Felix Lazarev
+*/
 
 #include "freedo_diag_port.h"
 
@@ -32,64 +32,64 @@ static uint16_t SendPTR;
 
 void _diag_Send(unsigned int val)
 {
-   if(GetPTR!=16)
-   {
+  if(GetPTR!=16)
+    {
       GetPTR=16;
       SendPTR=16;
       SNDDebugFIFO0=0;
       SNDDebugFIFO1=0;
-   }
+    }
 
-   SNDDebugFIFO0|=(val&1)<<(SendPTR-1);
-   SNDDebugFIFO1|=((val&1)>>1)<<(SendPTR-1);
+  SNDDebugFIFO0|=(val&1)<<(SendPTR-1);
+  SNDDebugFIFO1|=((val&1)>>1)<<(SendPTR-1);
 
-   SendPTR--;
+  SendPTR--;
 
-   if(SendPTR==0)
-      SendPTR=16;
+  if(SendPTR==0)
+    SendPTR=16;
 }
 
 unsigned int _diag_Get()
 {
-   unsigned int val = 0;
+  unsigned int val = 0;
 
-   if(SendPTR!=16)
-   {
+  if(SendPTR!=16)
+    {
       GetPTR=16;
       SendPTR=16;
-   }
+    }
 
-   val=((RCVDebugFIFO0>>(GetPTR-1))&0x1);
-   val|=((RCVDebugFIFO1>>(GetPTR-1))&0x1)<<0x1;
-   GetPTR--;
+  val=((RCVDebugFIFO0>>(GetPTR-1))&0x1);
+  val|=((RCVDebugFIFO1>>(GetPTR-1))&0x1)<<0x1;
+  GetPTR--;
 
-   /*	if(GetPTR>0)
-      {
-      RCVDebugFIFO0=RCVDebugFIFO0>>1;
-      RCVDebugFIFO1=RCVDebugFIFO1>>1;
-      GetPTR--;
-      val=RCVDebugFIFO0&0x1;
-      val|=(RCVDebugFIFO1&0x1)<<0x1;
-      }
-      */
-   if(GetPTR==0)  GetPTR=16;
+  /*	if(GetPTR>0)
+        {
+        RCVDebugFIFO0=RCVDebugFIFO0>>1;
+        RCVDebugFIFO1=RCVDebugFIFO1>>1;
+        GetPTR--;
+        val=RCVDebugFIFO0&0x1;
+        val|=(RCVDebugFIFO1&0x1)<<0x1;
+        }
+  */
+  if(GetPTR==0)  GetPTR=16;
 
-   return val;
+  return val;
 }
 
 void _diag_Init(int testcode)
 {
-   SNDDebugFIFO0=0;
-   SNDDebugFIFO1=0;
-   GetPTR=16;
-   SendPTR=16;
+  SNDDebugFIFO0=0;
+  SNDDebugFIFO1=0;
+  GetPTR=16;
+  SendPTR=16;
 
-   if(testcode>=0)
-   {
+  if(testcode>=0)
+    {
       testcode ^= 0xFF;
       testcode |= 0xA000;
-   } else testcode=0;
+    } else testcode=0;
 
-   RCVDebugFIFO0=testcode;
-   RCVDebugFIFO1=testcode;
+  RCVDebugFIFO0=testcode;
+  RCVDebugFIFO1=testcode;
 }
