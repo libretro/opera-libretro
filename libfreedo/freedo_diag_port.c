62a82fa89ca3af7512bc7510cf11e680edb01325
Antonio SJ Musumeci <trapexit@spawn.link>
2018-05-06 18:03:11 -0400

    cleanup and restructure diag_port

--- libfreedo/freedo_diag_port.c
+++ libfreedo/freedo_diag_port.c
@@ -34,71 +34,81 @@ static uint16_t SNDDebugFIFO0;
 static uint16_t SNDDebugFIFO1;
 static uint16_t RCVDebugFIFO0;
 static uint16_t RCVDebugFIFO1;
-static uint16_t GetPTR;
-static uint16_t SendPTR;
+static uint16_t GetIdx;
+static uint16_t SendIdx;
 
-void freedo_diag_port_send(const uint32_t val_)
-{
-  if(GetPTR!=16)
-    {
-      GetPTR=16;
-      SendPTR=16;
-      SNDDebugFIFO0=0;
-      SNDDebugFIFO1=0;
-    }
-
-  SNDDebugFIFO0|=(val_&1)<<(SendPTR-1);
-  SNDDebugFIFO1|=((val_&1)>>1)<<(SendPTR-1);
-
-  SendPTR--;
-
-  if(SendPTR==0)
-    SendPTR=16;
-}
-
-uint32_t freedo_diag_port_get(void)
-{
-  unsigned int val = 0;
-
-  if(SendPTR!=16)
-    {
-      GetPTR=16;
-      SendPTR=16;
-    }
-
-  val=((RCVDebugFIFO0>>(GetPTR-1))&0x1);
-  val|=((RCVDebugFIFO1>>(GetPTR-1))&0x1)<<0x1;
-  GetPTR--;
-
-  /*	if(GetPTR>0)
-        {
-        RCVDebugFIFO0=RCVDebugFIFO0>>1;
-        RCVDebugFIFO1=RCVDebugFIFO1>>1;
-        GetPTR--;
-        val=RCVDebugFIFO0&0x1;
-        val|=(RCVDebugFIFO1&0x1)<<0x1;
-        }
-  */
-  if(GetPTR==0)  GetPTR=16;
-
-  return val;
-}
-
-void freedo_diag_port_init(const int32_t test_code_)
+void
+freedo_diag_port_init(const int32_t test_code_)
 {
   int32_t test_code = test_code_;
-  
-  SNDDebugFIFO0=0;
-  SNDDebugFIFO1=0;
-  GetPTR=16;
-  SendPTR=16;
 
-  if(test_code>=0)
+  GetIdx  = 16;
+  SendIdx = 16;
+  SNDDebugFIFO0 = 0;
+  SNDDebugFIFO1 = 0;
+
+  if(test_code >= 0)
     {
       test_code ^= 0xFF;
       test_code |= 0xA000;
-    } else test_code=0;
+    }
+  else
+    {
+      test_code = 0;
+    }
 
-  RCVDebugFIFO0=test_code;
-  RCVDebugFIFO1=test_code;
+  RCVDebugFIFO0 = test_code;
+  RCVDebugFIFO1 = test_code;
+}
+
+void
+freedo_diag_port_send(const uint32_t val_)
+{
+  if(GetIdx != 16)
+    {
+      GetIdx  = 16;
+      SendIdx = 16;
+      SNDDebugFIFO0 = 0;
+      SNDDebugFIFO1 = 0;
+    }
+
+  SNDDebugFIFO0 |= ((val_ & 1) << (SendIdx - 1));
+  SNDDebugFIFO1 |= (((val_ & 1) >> 1) << (SendIdx - 1));
+
+  SendIdx--;
+
+  if(SendIdx == 0)
+    SendIdx = 16;
+}
+
+uint32_t
+freedo_diag_port_get(void)
+{
+  unsigned int val = 0;
+
+  if(SendIdx != 16)
+    {
+      GetIdx  = 16;
+      SendIdx = 16;
+    }
+
+  val  = ((RCVDebugFIFO0 >> (GetIdx - 1)) & 0x1);
+  val |= (((RCVDebugFIFO1 >> (GetIdx - 1)) &0x1 ) << 0x1);
+  GetIdx--;
+
+  /*
+    if(GetIdx > 0)
+    {
+      RCVDebugFIFO0 = (RCVDebugFIFO0 >> 1);
+      RCVDebugFIFO1 = (RCVDebugFIFO1 >> 1);
+      GetIdx--;
+      val  = (RCVDebugFIFO0 & 0x1);
+      val |= ((RCVDebugFIFO1 & 0x1) << 0x1);
+    }
+  */
+
+  if(GetIdx == 0)
+    GetIdx = 16;
+
+  return val;
 }
