974421617de9691eaa0e220c87e115982d31643a
Antonio SJ Musumeci <trapexit@spawn.link>
2026-05-31 22:16:13 -0500

    cdrom: add ODE protocol and timed media reads

--- libopera/opera_xbus_cdrom_plugin.c
+++ libopera/opera_xbus_cdrom_plugin.c
@@ -26,7 +26,10 @@ xbus_cdrom_plugin(int   proc_,
       opera_cdrom_send_cmd(&g_CDROM_DEVICE,(uint8_t)(uintptr_t)data_);
       break;
     case XBP_FIQ:
-      return (void*)(uintptr_t)opera_cdrom_test_fiq(&g_CDROM_DEVICE);
+      return (void*)(uintptr_t)opera_cdrom_update_fiq(&g_CDROM_DEVICE);
+    case XBP_SET_DATA:
+      opera_cdrom_fifo_set_data(&g_CDROM_DEVICE,(uint8_t)(uintptr_t)data_);
+      break;
     case XBP_GET_DATA:
       return (void*)(uintptr_t)opera_cdrom_fifo_get_data(&g_CDROM_DEVICE);
     case XBP_GET_STATUS:
@@ -35,13 +38,16 @@ xbus_cdrom_plugin(int   proc_,
       opera_cdrom_set_poll(&g_CDROM_DEVICE,(uint32_t)(uintptr_t)data_);
       break;
     case XBP_GET_POLL:
+      opera_cdrom_update_fiq(&g_CDROM_DEVICE);
       return (void*)(uintptr_t)g_CDROM_DEVICE.poll;
     case XBP_GET_SAVESIZE:
-      return (void*)(uintptr_t)opera_state_save_size(sizeof(cdrom_device_t));
+      return (void*)(uintptr_t)opera_cdrom_state_size();
     case XBP_GET_SAVEDATA:
-      return (void*)(uintptr_t)opera_state_save(data_,"CDRM",&g_CDROM_DEVICE,sizeof(g_CDROM_DEVICE));
+      return (void*)(uintptr_t)opera_cdrom_state_save(&g_CDROM_DEVICE,data_);
     case XBP_SET_SAVEDATA:
-      return (void*)(uintptr_t)opera_state_load(&g_CDROM_DEVICE,"CDRM",data_,sizeof(g_CDROM_DEVICE));
+      return (void*)(uintptr_t)opera_cdrom_state_load(&g_CDROM_DEVICE,
+                                                      ((opera_xbus_savedata_t const*)data_)->data,
+                                                      ((opera_xbus_savedata_t const*)data_)->size);
     };
 
   return NULL;
